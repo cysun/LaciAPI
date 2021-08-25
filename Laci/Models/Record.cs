6326ad84fe2783d9fb85e7aefabdb62676c260c4
Chengyu Sun <csun@calstatela.edu>
2021-08-25 10:42:38 -0700

    Add project files.

---
+++ Laci/Models/Record.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laci.Models
+{
+    public class Record
+    {
+        public int CityId { get; set; }
+        public City City { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        public int? Tests { get; set; }
+        public int? Cases { get; set; }
+        public int? Deaths { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# LACI Web API
+
+This is a re-implementation of [LACI](https://github.com/cysun/Laci) using a web API and a SPA.
